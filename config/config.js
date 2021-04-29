f1bfd557d2228757d9624d7871afe22f70f4e24b
leegyusung <lgs7542@naver.com>
2021-04-29 12:48:45 +0900

    배포6

--- config/config.js
+++ config/config.js
@@ -16,11 +16,12 @@ module.exports = {
         dialect: "mysql"
     },
     production: {
-        username: "root",
-        password: process.env.SEQUELIZE_PASSWORD,
+        username: "be8aab0bdba387",
+        password: "015e2c77",
         database: "Shop",
-        host: "127.0.0.1",
+        host: "us-cdbr-east-03.cleardb.com",
         dialect: "mysql",
         //logging:false,
     }
+   // mysql://be8aab0bdba387:015e2c77@us-cdbr-east-03.cleardb.com/heroku_5954aec46fd3766?reconnect=true
 }
