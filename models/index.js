db57aef8edc1243f661f1f4dfbfa8223e2ef9ec0
leegyusung <lgs7542@naver.com>
2021-04-29 13:45:53 +0900

    배포8

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const Wishlist = require('./wishlist');
 const Purchaselist = require('./purchaselist');
 const PurChaseComment = require('./purChaseComment');
 
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'production';
 const config = require('../config/config')[env];
 const db = {};
 
